c47a5ee32cdbc90fb504458670370740a8c2f74f
Jonathan Settle <jdsettle@gmail.com>
2015-10-14 22:18:54 -0400

    attempted to complete again - still have an error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,14 @@
 class CommentsController < ApplicationController
   def new
     @topic = Topic.find(params[:topic_id])
-    @post = Post.find(params[:post_id])
+    @post = @topic.posts.find(params[:post_id])
     @comment = Comment.new
     authorize @comment
   end
 
   def create
     @topic = Topic.find(params[:topic_id])
-    @post = Post.find(params[:post_id])
+    @post = @topic.posts.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
     @comment.user = current_user
     authorize @comment
@@ -38,6 +38,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body, :user_id)
   end
 end
