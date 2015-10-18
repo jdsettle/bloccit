357a414711226b9899966d6499aa3e43bad739ad
Jonathan Settle <jdsettle@gmail.com>
2015-10-18 11:33:48 -0400

    Progress on voting checkpoint, need to merge destroy assignment to truly move forward, was waiting on mentor approval but haven't heard..

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe Post do
-  describe "vote methods do"
+  describe "vote methods" do
 
     before do
-      @post = Post.create(title: 'post title', body: 'post body')
+      @post = Post.create(title: 'post title', body: 'post bodies must be pretty long')
       3.times { @post.votes.create(value: 1) }
       2.times { @post.votes.create(value: -1) }
     end
@@ -26,6 +26,5 @@ describe Post do
         expect(@post.points).to eq(1) # 3 - 2
       end
     end
-
   end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 describe Vote do
   describe "validations" do
+
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        expect(@vote.value).to eq(-1) || expect(@vote.value).to eq(1)
+        invalid_vote = Vote.new(value: 2)
+        expect(invalid_vote.valid?).to eq(false)
       end
     end
+    
   end
 end
